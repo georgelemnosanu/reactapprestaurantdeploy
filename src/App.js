1397e60fe9770bbe3d9fefa99030c5bf47d92a22
LMN <georgelemnosanu@gmail.com>
2023-10-20 23:24:58 +0300

    lasttest

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import ViewAllSpeciality from './ViewAllSpeciality';
 import MenuApp from './MenuApp';
 import CreateSpeciality from './CreateSpeciality';
