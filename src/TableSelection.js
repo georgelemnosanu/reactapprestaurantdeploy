fa573b1419b4cf99a5beae69a5b4d6aefbb92f17
GeorgeLMN <georgelemnosanu@gmail.com>
2024-11-23 15:42:58 +0200

    testfinal

--- src/TableSelection.js
+++ src/TableSelection.js
@@ -26,7 +26,7 @@ function TableSelection() {
         {tableData.map(table => (
           <li key={table.id}>
             {/* Use <Link> to create links */}
-            <a href={`/table/${table.id}/menu/${menuId}`} onClick={() => handleTableSelect(table.id)}>
+            <a href={`/api/table/${table.id}/menu/${menuId}/specialities`} onClick={() => handleTableSelect(table.id)}>
               {table.tableName}
             </a>
           </li>
